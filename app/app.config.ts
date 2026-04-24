e8a589e601cfbe91858b1b1e3175a5fc9070c1a7
Qries <32694477+qqxpee@users.noreply.github.com>
2026-04-24 19:25:30 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   title: 'Sink',
-  github: 'https://github.com/miantiao-me/sink',
-  coffee: 'https://sink.cool/coffee',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
+  github: 'https://github.com/qqxpee/Sink',
+  coffee: 'https://p.pee.ee',
+  twitter: 'https://p.pee.ee',
+  telegram: 'https://p.pee.ee',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://p.pee.ee/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
