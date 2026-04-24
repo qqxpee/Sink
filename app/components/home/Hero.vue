d59aea02adaf56fb3884cef36b40bb7ad49c1b6f
Qries <32694477+qqxpee@users.noreply.github.com>
2026-04-24 19:47:31 +0800

    Update Hero.vue

--- app/components/home/Hero.vue
+++ app/components/home/Hero.vue
@@ -4,11 +4,7 @@
 <template>
   <section 
     class="h-screen w-full bg-cover bg-center bg-no-repeat"
-    style="background-image: url('https://img.pee.ee/i/me/hq.png');"
+    style="background-image: url('https://img.pee.ee/i/me/hq.png'); position: fixed; top: 0; left: 0; z-index: 99999999999;"
   >
-    <div class="h-full w-full py-16 md:py-24">
-      <div class="mx-auto flex h-full max-w-6xl flex-col items-center gap-12 px-6 lg:flex-row lg:justify-between">
-        </div>
-    </div>
   </section>
 </template>
